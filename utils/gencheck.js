e86e9922b6304e905eca9ebff2d35dca424f643d
Yunoxa <agentreborn3@gmail.com>
2024-09-02 02:13:14 +0100

    Removed unnecessary requires in gencheck

--- utils/gencheck.js
+++ utils/gencheck.js
@@ -1,7 +1,5 @@
-const fs = require("fs");
-const Eris = require("eris");
-const Constants = Eris.Constants;
 const generators = require("./generators/sentence")
+
 async function checkGeneratorType(interaction) {
   if(interaction.data.options) {
     for(const generator in generators) {
